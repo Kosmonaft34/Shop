1f25505e791364e208533d45f1f955a3a48ce385
Maxim Ukolov <maksimukoloff@yandex.ru>
2021-10-18 18:49:23 +0300

    methods in UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,37 @@ class UserController extends Controller
     public function index(){
         return view('users.registers');
     }
+    //регистрация пост запрос
+    public function create(){
+
+    }
+    //view страницы с авторизацией
+    public function logout(){
+        return view('users.logout');
+    }
+    //авторизация пост запрос
+    public function auth(){
+
+    }
+
+    //view страницы личного кабинета
+    public function private(){
+
+    }
+
+    //view страницы мой профиль
+    public function profile(){
+
+    }
+
+    //view страницы мои заказы
+    public function orders(){
+
+    }
+
+    //view страницы корзины
+    public function basket(){
+
+    }
+
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('Главная страница')</title>
+    <title>@yield('title','Главная страница')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <nav>
@@ -15,8 +16,9 @@
         <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
     </div>
 </nav>
+@yield('content')
 @section('script')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @show
 </body>
 </html>
